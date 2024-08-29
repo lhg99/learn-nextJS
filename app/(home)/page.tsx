89b90300811df7fbacd2ec69a7476222ee01db5f
lhg99 <lee991229@naver.com>
2024-08-29 12:20:20 +0900

    노마드 코더 강의내용

---
+++ app/(home)/page.tsx
@@ -0,0 +1,44 @@
+// "use client";
+
+import Link from "next/link";
+import { resolve } from "path";
+
+export const metadata = {
+    title: "Home",
+}
+
+export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+
+async function getMovies() {
+    // await new Promise((resolve) => setTimeout(resolve, 1000));
+    const response = await fetch(API_URL);
+    const json = await response.json();
+    return json;
+} 
+
+export default async function HomePage() {
+    const movies = await getMovies();
+    return (
+        <div>
+            {movies.map(movie => <li key={movie.id}><Link href={`/movies/${movie.id}`}>{movie.title}</Link></li>)}
+        </div>
+    )
+}
+
+// const [movies, setMovies] = useState([]);
+//     const [isLoading, setIsLoading] = useState(true);
+//     const getMovies = async() => {
+//         const response = await fetch("https://nomad-movies.nomadcoders.workers.dev/movies")
+//         const json = await response.json();
+//         setMovies(json);
+//         setIsLoading(false)
+//     }
+
+//     useEffect(() => {
+//         getMovies();
+//     }, []);
+//     return (
+//         <div>
+//             <h1>{isLoading ? "loading" : JSON.stringify(movies)}</h1>
+//         </div>
+//     )
---
+++ app/about-us/layout.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from "next"
+
+
+
+export default function RootLayout({children,}: {children: React.ReactNode}) {
+  return (
+    <div>
+        {children}
+        &copy; Next JS is great!
+    </div>
+  )
+}
